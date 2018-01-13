dba26d232e96d395648f211da0c0c58ab113cafc
sejig2017 <34083177+sejig2017@users.noreply.github.com>
2018-01-13 17:21:57 -0500

    perifericos ocultar cuando sea pantalla

--- util/conexion2.php
+++ util/conexion2.php
@@ -1,6 +1,7 @@
 <?php 
 function getConn(){
-  $mysqli = mysqli_connect('localhost', 'root', 'Soporte', "soporte");
+//  $mysqli = mysqli_connect('localhost', 'root', 'Soporte', "soporte");
+  $mysqli = mysqli_connect('192.168.1.4', 'root', 'Soporte', "soporte");
   if (mysqli_connect_errno($mysqli))
     echo "Fallo al conectar a MySQL: " . mysqli_connect_error();
   $mysqli->set_charset('utf8');
