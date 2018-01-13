0248c45ce5991ed510fe621ce79fdbb40ef898fa
DiegoCV <skdiegomxtr@gmail.com>
2018-01-13 17:04:48 -0500

    Modifique insertar

--- controler/periferico/insertar.php
+++ controler/periferico/insertar.php
@@ -1,19 +1,24 @@
 <?php
+require_once '../../dao/PerifericoDao.php.php';
+include_once '../../dto/Perifericos.php.php';
+
 /**
          *Insertar un perifericos. Se trae todo. La validación se hace en HTML y aquí llegan muuuchos ""      
          *La request debe traer idEquipo,idTipoPeriferico,idTipoPantalla,marca,modelo,serial,pulgadas,sticker_activo,fecha_compra         
          */        
             $perifericos=new Perifericos();
-            $equipo=new Equipo();
-            $equipo->setIdEquipo(filter_input(INPUT_POST,'idEquipo'));
-            $perifericos->setEquipoequipo($equipo);            
-            $tipoPeriferico=new TipoPeriferico();
-            $tipoPeriferico->setIdTipoPeriferico(filter_input(INPUT_POST,'idTipoPeriferico'));
-            $perifericos->setTipoPeriferico($tipoPeriferico);
-            $tipoPantalla=new TipoPantalla();
-            $tipoPantalla->setIdTipoPantalla(filter_input(INPUT_POST,'idTipoPantalla'));            
-            $perifericos->setTipoPantallatipoPantalla($tipoPantalla);
+            $perifericoDao = new PerifericoDao();
             
+            $perifericos->setEquipoequipo($equipo);  
+            $tipoPeriferico=new TipoPeriferico();         
+            $tipoPeriferico->setIdTipoPeriferico(filter_input(INPUT_POST,'idTipoPeriferico'));            
+            $perifericos->setTipoPeriferico($tipoPeriferico);
+            
+            
+            $tipoPantalla=new TipoPantalla();         
+            $tipoPantalla->setIdTipoPantalla(filter_input(INPUT_POST,'idTipoPantalla')); 
+            
+            $perifericos->setTipoPantallatipoPantalla($tipoPantalla);          
             $perifericos->setMarca(filter_input(INPUT_POST,'marca'));
             $perifericos->setModelo(filter_input(INPUT_POST,'modelo'));
             $perifericos->setSerial(filter_input(INPUT_POST,'serial'));
@@ -21,4 +26,4 @@
             $perifericos->setPulgadas(filter_input(INPUT_POST,'pulgadas'));
             $perifericos->setFechaCompra(filter_input(INPUT_POST,'fecha_compra'));            
             // guardar en dao                   
-
+            $perifericoDao->guardar($periferico);
