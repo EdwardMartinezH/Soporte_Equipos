966f84e65d784356166f436b4ddd2b3a9f5c6f52
Fredy Arciniegas <32426482+elcaballerodelanoche@users.noreply.github.com>
2018-01-15 20:30:46 -0500

    Reparación de un bug
    El select no pintaba el primer elemento

--- controllers/CargoList.php
+++ controllers/CargoList.php
@@ -6,7 +6,7 @@ function getListasRep(){
     $cargos = CargoController::listAll();
 $html = "<option value=''>ELIJA UNA OPCION </option>";
 for ($i = 0; $i < count($cargos); $i++) {
-    if ($i > 0) {
+    if ($cargos[$i]->getId() > 0) {
         $id = $cargos[$i]->getId();
         $nombre = $cargos[$i]->getNombre();
         $html .= "<option value='$id'>$id . $nombre</option>";
