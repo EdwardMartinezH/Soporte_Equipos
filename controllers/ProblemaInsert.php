d32e24fb5703bf1c620cfc500beab45f4ecea1e3
Fredy Arciniegas <32426482+elcaballerodelanoche@users.noreply.github.com>
2018-01-20 16:07:22 -0500

    conexión a vista

--- controllers/ProblemaInsert.php
+++ controllers/ProblemaInsert.php
@@ -12,13 +12,10 @@ $ex="";
             
             
             $equipo= EquipoController::selectByUsuario($idUsuario);
-            if($equipo!=null){                            
-            try{                
-                                   
-            $problema=(filter_input(INPUT_POST,'problema'));            
-            
-            ProblemaController::insert("", $problema, $equipo->getIdEquipo(),"");            
-            
+            if($equipo!=null && $equipo->getIdEquipo()!=null){                            
+            try{                                                   
+            $descproblema=(filter_input(INPUT_POST,'problema'));                        
+            ProblemaController::insert("", $descproblema, $equipo->getIdEquipo(),"");                        
             $img=filter_input(INPUT_POST,'imagen');
             //ENVIAR ESO AL CORREO
             }catch(Exception $e){
@@ -31,34 +28,26 @@ $ex="";
             
 //---------------------------------------------------------            
             if($cargo == null){
-                echo '<script language="javascript">'.$ex.' window.location="../../vista/login.php"</script>';      
+                echo '<script language="javascript">'.$ex.' window.location="../vista/login.php"</script>';      
                        
                
-            }else{
-              
-                //var_dump($result);
+            }else{                              
      if($cargo=="2"||$cargo=="3"){//tipo director 1
          $_SESSION['cargo_id']= $cargo; 
          $_SESSION['id_usuario'] = $idUsuario;
          $_SESSION['nombre_usuario'] = $nombreUsuario;
 
 //         var_dump($cargo.$idUsuario. $nombreUsuario);
-          echo '<script language="javascript">'.$ex.'window.location="../../vista/formPrincipal_gerencia.php"</script>';
+          echo '<script language="javascript">'.$ex.'window.location="../vista/formPrincipal_gerencia.php"</script>';
 
-     }
-//     if($cargo==2||$usuario->getCargo_id()==3){//tipo director 1
-//           $_SESSION['basededatos'] = $usuario->getCargo_id();
-//          echo '<script language="javascript">alert("'.$usuario->getCargo_id().'");window.location="../../vista/formPrincipal_gerencia.php"</script>';
-//          
-//     }
-     
+     }     
      if($cargo=="29"){//tipo director 1
          $_SESSION['cargo_id']= $cargo; 
          $_SESSION['id_usuario'] = $idUsuario;
          $_SESSION['nombre'] = $nombreUsuario;
 
            
-          echo '<script language="javascript">'.$ex.'window.location="../../vista/formPrincipal_Adm.php"</script>';
+          echo '<script language="javascript">'.$ex.'window.location="../vista/formPrincipal_Adm.php"</script>';
           
      } 
      else {         
@@ -67,7 +56,6 @@ $ex="";
          $_SESSION['nombre_usuario'] = $nombreUsuario;
            
 //            var_dump($cargo.$idUsuario. $nombreUsuario);
-           echo '<script language="javascript">'.$ex.'window.location="../../vista/formPrincipal.php"</script>';
+           echo '<script language="javascript">'.$ex.'window.location="../vista/formPrincipal.php"</script>';
      }
             }
-            
